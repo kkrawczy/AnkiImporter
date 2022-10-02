d329f8a9ed5ee799d9bdad130860bb81d9f8c7df
karol <kkrawczyk@edgelab.ch>
2022-10-02 10:45:59 +0200

    update versions

--- src/main/java/com/karol/AmazonService.java
+++ src/main/java/com/karol/AmazonService.java
@@ -1,6 +1,10 @@
 package com.karol;
 
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.polly.AmazonPolly;
 import com.amazonaws.services.polly.AmazonPollyClient;
+import com.amazonaws.services.polly.AmazonPollyClientBuilder;
 import com.amazonaws.services.polly.model.*;
 
 import java.io.InputStream;
@@ -14,11 +18,14 @@ public class AmazonService {
     private static final String VOICE_JOANNA = "Joanna";
 
 
-    AmazonPollyClient client = new AmazonPollyClient();
+    AmazonPolly client = AmazonPollyClientBuilder.standard()
+        .withRegion(Regions.DEFAULT_REGION)
+        .withCredentials( new ProfileCredentialsProvider("private"))
+        .build();
 
     public InputStream generateAudioForText(String text) {
         SynthesizeSpeechRequest request = new SynthesizeSpeechRequest()
-                .withEngine(Engine.Neural)
+
                 .withTextType(TextType.Ssml)
                 .withText("<speak><prosody rate=\"slow\">"+text+"</prosody></speak>")
                 .withVoiceId(VOICE_MATTHEW)
